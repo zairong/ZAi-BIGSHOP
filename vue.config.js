fd5ea0c81d1918bb66c34419b34bea64d01742d8
Zairong <justiceman3211@hotmail.com>
2022-12-06 21:44:47 +0800

    大型電商專案

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  productionSourceMap:false,
+  // 關閉ESLINT校驗工具
+  lintOnSave: false,
+  //配置代理跨域
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://gmall-h5-api.atguigu.cn",
+      },
+    },
+  },
+};
